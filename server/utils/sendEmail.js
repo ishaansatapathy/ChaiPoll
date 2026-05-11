cc8d0c7e50cf87f7a3e87650de8c534a2faaebd8
Ishaan Satapathy <ishaansatapathy09.com>
2026-05-12 00:53:50 +0530

    Production: Simplified Gmail SMTP using built-in service abstraction

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -3,15 +3,10 @@ import nodemailer from 'nodemailer';
 const sendEmail = async (options) => {
   try {
     const transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
-      port: 587,
-      secure: false, 
+      service: 'gmail',
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
-      },
-      tls: {
-        rejectUnauthorized: false
       }
     });
 
